3e6cf003a3afb6c81c060d8187dc8f3b9648bea9
Ubuntu <ubuntu@ip-172-31-5-69.ap-south-1.compute.internal>
2022-05-16 16:07:22 +0000

    test

---
+++ abc.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+set -e
+
+r_d(){
+	if [ $4 == "deepak" ]
+	then
+		port=$1
+	else
+		port=$2
+	fi
+	echo "this is $port"
+}
+
+case "$b_name" in
+	dev)	r_d 123 456 $b_name $instance;;
+	qa)    r_d 1234 4567 $b_name $instance;;
+	prod)    r_d 12345 45678 $b_name $instance;;
+	*) 	echo "unknow"
+esac
